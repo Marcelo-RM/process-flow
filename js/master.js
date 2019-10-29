75de3c06021fdadf8e0aebaa00f8a74c8561d413
Marcelo Rodrigues Montalvão <mmontalvao@uniaoquimica.com.br>
2019-10-29 11:00:57 -0300

    changing infoText step-progress

--- js/master.js
+++ js/master.js
@@ -1,4 +1,34 @@
-function showInfo(event) {
-    debugger;
-    var place = document.getElementById("infoPlace");
+window.onload = function() {
+    // Create click function to status items
+    var listItems = document.querySelectorAll("ul.progressbar li");
+    listItems.forEach(item => {
+        item.onclick = changeColor;
+    });
+
+    //Set info for the first item in panel
+    this.updateInfo(listItems[0]);
+}
+
+function changeColor(event){
+    var className = event.target.className;
+    if (className != "selected"){
+        removeSelectedClass();
+        event.target.className = "selected";
+        updateInfo(event.target);
+    }
+}
+
+function removeSelectedClass(){
+    var status = document.querySelectorAll("ul.progressbar li");
+    status.forEach(item => {
+        item.className = "";
+    });
+}
+
+function updateInfo(item){
+    var head = document.getElementById("panelHeading");
+    var cont = document.getElementById("panelContent");
+
+    head.innerText = item.innerText;
+    cont.innerText = "Exibindo conteudo referente ao " + item.innerText;
 }
