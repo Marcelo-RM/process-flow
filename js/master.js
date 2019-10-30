2ea765291094389f535a8318359bb2b3d7fd0f91
Marcelo Rodrigues Montalvão <mmontalvao@uniaoquimica.com.br>
2019-10-30 14:31:38 -0300

    Adicionado alguns icones

--- js/master.js
+++ js/master.js
@@ -9,15 +9,29 @@ window.onload = function() {
     this.updateInfo(listItems[0]);
 }
 
+/**
+ * Método usado para alterar cor (classe) quando item for clicado
+ * @param {MouseEvent} event 
+ */
 function changeColor(event){
     var className = event.target.className;
+    var listItem = event.target;
+
+    //Se a classe conter fas o clique foi no icone
+    //É necessário alterar classe parent
+    if(className.includes("fas")){
+        listItem = event.target.parentElement;
+    }
     if (className != "selected"){
         removeSelectedClass();
-        event.target.className = "selected";
-        updateInfo(event.target);
+        listItem.className = "selected";
+        updateInfo(listItem);
     }
 }
 
+/**
+ * Método que remove a classe de todos os itens de status
+ */
 function removeSelectedClass(){
     var status = document.querySelectorAll("ul.progressbar li");
     status.forEach(item => {
@@ -25,6 +39,10 @@ function removeSelectedClass(){
     });
 }
 
+/**
+ * Altera o conteúdo da panel de informação
+ * @param {HtmlObject} item Status clicado
+ */
 function updateInfo(item){
     var head = document.getElementById("panelHeading");
     var cont = document.getElementById("panelContent");
