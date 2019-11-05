1f08efc38422c2e61b44313975570a471651db6f
Marcelo-RM <marcelordrgs98@gmail.com>
2019-11-05 10:07:09 -0200

    correção grafico incremental

--- js/master.js
+++ js/master.js
@@ -119,6 +119,224 @@ function createListItem(item, status){
     return listItem;
 }
 
+/**
+ * Metodo chamado durante clique no botão pesquisar
+ * Será substituido por um método c# para consulta ao banco de dados
+ * Criar método que chame uma função c# pode ser a melhor solução
+ */
+function updateChart(){
+    var cnpj = document.getElementById("cnpj");
+    if(cnpj.value == ""){
+        cnpj.style.border = '1px solid red';
+        //return;
+    }
+    else {
+        cnpj.style.border = '1px solid #ccc';
+    }
+    var listItems = document.querySelectorAll("ul.progressbar li");
+    var items = getItems();
+
+    var qtdRecusado = 0, qtdCriado = 0, qtdValidacao = 0, qtdSeparacao = 0,
+        qtdFaturado = 0, qtdTransito = 0, qtdEntregue = 0;
+    //Loop para popular valores referentes aos dados do grafico
+    items.forEach(function(el){
+        if(el.status == "criado" && el.sts == "recusado"){
+            qtdRecusado += 1;
+        }
+        if(el.status == "criado"){
+            qtdCriado += 1;
+        }
+        if(el.status == "validacao"){
+            qtdCriado += 1;
+            qtdValidacao += 1;
+        }
+        if(el.status == "separacao"){
+            qtdCriado += 1;
+            qtdValidacao += 1;
+            qtdSeparacao += 1;
+        }
+        if(el.status == "faturado"){
+            qtdCriado += 1;
+            qtdValidacao += 1;
+            qtdSeparacao += 1;
+            qtdFaturado += 1;
+        }
+        if(el.status == "transito"){
+            qtdCriado += 1;
+            qtdValidacao += 1;
+            qtdSeparacao += 1;
+            qtdFaturado += 1;
+            qtdTransito += 1;
+        }
+        if(el.status == "entregue"){
+            qtdCriado += 1;
+            qtdValidacao += 1;
+            qtdSeparacao += 1;
+            qtdFaturado += 1;
+            qtdTransito += 1;
+            qtdEntregue += 1;
+        }
+    });
+    //Loop que vai criar os graficos para cada item
+    listItems.forEach(function(item){
+        var greenSize = 0;
+        var graySize = 0;
+        var redSize = qtdRecusado;
+
+        if(item.id == "criado"){
+            greenSize = qtdCriado;
+            graySize = items.length - greenSize;
+        }
+        if(item.id == "validacao"){
+            greenSize = qtdValidacao;
+            graySize = items.length - (greenSize + redSize);
+            redSize = 0;
+        }
+        if(item.id == "separacao"){
+            greenSize = qtdSeparacao;
+            graySize = items.length - (greenSize + redSize);
+            redSize = 0;
+        }
+        if(item.id == "faturado"){
+            greenSize = qtdFaturado;
+            graySize = items.length - (greenSize + redSize);
+            redSize = 0;
+        }
+        if(item.id == "transito"){
+            greenSize = qtdTransito;
+            graySize = items.length - (greenSize + redSize);
+            redSize = 0;
+        }
+        if(item.id == "entregue"){
+            greenSize = qtdEntregue;
+            graySize = items.length - (greenSize + redSize);
+            redSize = 0;
+        }
+
+        var elem = item.children[0].tagName == "DIV" ? item.children[1] : item.children[0];
+        createChart(elem, greenSize, graySize, redSize);
+    })
+    changeColor(listItems[0]);
+}
+
+/**
+ * Método usado para criação de gráfico do tipo rosca
+ * @param {HtmlObject} elem Objeto html em que ficará o gráfico
+ * @param {Number} vl1 Valor exibido no gráfico com a cor verde
+ * @param {Number} vl2 Valor exibido no gráfico com a cor cinza
+ */
+function createChart(elem, valorVerde, valorCinza, valorVermelho) {
+    //Verifica se ha valor vermelho
+    if(!valorVermelho){valorVermelho = 0}
+    //validar se está recebendo elemento para criação do grafico
+    var ctx = elem != undefined ? elem.getContext('2d') : "";
+    if(ctx == ""){return;}
+
+    //Create Chart
+    var chart = new Chart(ctx, {
+        type: 'doughnut', //tipo do grafico
+        data: {
+            datasets: [{
+                data: [
+                    valorVerde, //Quantidade de itens de cor verde
+                    valorCinza, //Quantidade de itens de cor cinza
+                    valorVermelho  //Quantidade de itens de cor vermelha
+                ],
+                backgroundColor: [
+                    '#3ada3a', //verde
+                    '#aaa', //cinza
+                    '#f02929' //Vermelho
+                ],
+                borderWidth: 0, //tamanho da borda
+                weight: 1
+            }]
+            /*,
+            labels: [
+                'Completo',
+                'Pendente'
+            ]*/
+        },
+        options: {
+            cutoutPercentage: 70, //Quanto maior o valor menor a largura do grafico
+            responsive: true, //Ajustar automaticamente ao tamanho do elemento pai
+            tooltips: {
+                enabled: false //Desativa tooltips pois não são necessários
+            }
+        }
+    });
+}
+
+function getItems(){
+    return [
+        {
+            material: 'material 001',
+            sts: 'ativo',
+            dataSep: '12/12/2018 12:00:00',
+            vendedor: 'Marcelo Montalvão',
+            numNFE: 12345,
+            dataNFE: '13/12/2018 15:00:00',
+            dataMinuta: '15/12/2018 09:30:00',
+            dataRecebimento: '15/12/2018 11:30:00',
+            status: 'validacao'
+        },{
+            material: 'material 002',
+            sts: 'ativo',
+            dataSep: '12/12/2018 12:00:00',
+            vendedor: 'Marcelo Montalvão',
+            numNFE: 12345,
+            dataNFE: '13/12/2018 15:00:00',
+            dataMinuta: '15/12/2018 09:30:00',
+            dataRecebimento: '15/12/2018 11:30:00',
+            status: 'validacao'
+        },{
+            material: 'material 003',
+            sts: 'ativo',
+            dataSep: '12/12/2018 12:00:00',
+            vendedor: 'Marcelo Montalvão',
+            numNFE: 12345,
+            dataNFE: '13/12/2018 15:00:00',
+            dataMinuta: '15/12/2018 09:30:00',
+            dataRecebimento: '15/12/2018 11:30:00',
+            status: 'faturado'
+        },{
+            material: 'material 004',
+            sts: 'ativo',
+            dataSep: '12/12/2018 12:00:00',
+            vendedor: 'Marcelo Montalvão',
+            numNFE: 12345,
+            dataNFE: '13/12/2018 15:00:00',
+            dataMinuta: '15/12/2018 09:30:00',
+            dataRecebimento: '15/12/2018 11:30:00',
+            status: 'validacao'
+        },{
+            material: 'material 005',
+            sts: 'ativo',
+            dataSep: '12/12/2018 12:00:00',
+            vendedor: 'Marcelo Montalvão',
+            numNFE: 12345,
+            dataNFE: '13/12/2018 15:00:00',
+            dataMinuta: '15/12/2018 09:30:00',
+            dataRecebimento: '15/12/2018 11:30:00',
+            status: 'separacao'
+        },{
+            material: 'material 006',
+            sts: 'recusado',
+            dataSep: '',
+            vendedor: 'Marcelo Montalvão',
+            dataMinuta: '',
+            dataRecebimento: '',
+            numNFE: '',
+            dataNFE: '',
+            status: 'criado'
+        }
+    ];
+}
+
+//Todas as funções abaixo criam templates html
+/**
+ * 
+ * @param {Object} item objeto contendo dados de um item especifico
+ */
 function detailForStatusEntregue(item){
     return "<li class='list-group-item row'>" +
     "<h4 class='mb-1'>"+ item.material +"</h4>" +
@@ -193,261 +411,4 @@ function detailForStatusCriado(item){
         "</div>" +
     "</div>" +
     "</li>";
-}
-
-/**
- * Metodo chamado durante clique no botão pesquisar
- * Será substituido por um método c# para consulta ao banco de dados
- * Criar método que chame uma função c# pode ser a melhor solução
- */
-function updateChart(){
-    var cnpj = document.getElementById("cnpj");
-    if(cnpj.value == ""){
-        cnpj.style.border = '1px solid red';
-        //return;
-    }
-    else {
-        cnpj.style.border = '1px solid #ccc';
-    }
-    var listItems = document.querySelectorAll("ul.progressbar li");
-    var items = getItems();
-
-    var qtdRecusado = 0, qtdCriado = 0, qtdValidacao = 0, qtdSeparacao = 0,
-        qtdFaturado = 0, qtdTransito = 0, qtdEntregue = 0;
-    items.forEach(function(el){
-        if(el.status == "criado" && el.sts == "recusado"){
-            qtdRecusado += 1;
-        }
-        if(el.status == "criado"){
-            qtdCriado += 1;
-        }
-        if(el.status == "validacao"){
-            qtdCriado += 1;
-            qtdValidacao += 1;
-        }
-        if(el.status == "separacao"){
-            qtdCriado += 1;
-            qtdValidacao += 1;
-            qtdSeparacao += 1;
-        }
-        if(el.status == "faturado"){
-            qtdCriado += 1;
-            qtdValidacao += 1;
-            qtdSeparacao += 1;
-            qtdFaturado += 1;
-        }
-        if(el.status == "transito"){
-            qtdCriado += 1;
-            qtdValidacao += 1;
-            qtdSeparacao += 1;
-            qtdFaturado += 1;
-            qtdTransito += 1;
-        }
-        if(el.status == "entregue"){
-            qtdCriado += 1;
-            qtdValidacao += 1;
-            qtdSeparacao += 1;
-            qtdFaturado += 1;
-            qtdTransito += 1;
-            qtdEntregue += 1;
-        }
-    });
-    listItems.forEach(function(item){
-        var greenSize = 0;
-        var graySize = 0;
-        var redSize = qtdRecusado;
-
-        if(item.id == "criado"){
-            greenSize = qtdCriado;
-            graySize = items.length - greenSize;
-        }
-        if(item.id == "validacao"){
-            redSize = 0;
-            greenSize = qtdValidacao;
-            graySize = items.length - greenSize;
-        }
-        if(item.id == "separacao"){
-            redSize = 0;
-            greenSize = qtdSeparacao;
-            graySize = items.length - greenSize;
-        }
-        if(item.id == "faturado"){
-            redSize = 0;
-            greenSize = qtdFaturado;
-            graySize = items.length - greenSize;
-        }
-        if(item.id == "transito"){
-            redSize = 0;
-            greenSize = qtdTransito;
-            graySize = items.length - greenSize;
-        }
-        if(item.id == "entregue"){
-            redSize = 0;
-            greenSize = qtdEntregue;
-            graySize = items.length - greenSize;
-        }
-
-        var elem = item.children[0].tagName == "DIV" ? item.children[1] : item.children[0];
-        createChart(elem, greenSize, graySize, redSize);
-    })
-    /*
-    listItems.forEach(item => {
-        //Change this; Creating charts for ; Cor verde
-        var size1 = items.filter(
-            function(el){
-                // var arrStatus = ['criado','validacao','separacao','faturado','transito','entregue'];
-                // var stsIndex = arrStatus.findIndex(function(elem){return elem == el.status});
-                // var stsIndex2 = arrStatus.findIndex(function(elem){return elem == item.id});
-                // debugger;
-                // if(stsIndex2 <= stsIndex){
-                //     return el.status;
-                // }
-                
-                if(el.status == item.id){
-                    return el.status
-                }
-            }).length;
-        //Verifica se há algum item recusado para exibição
-        var recusados = items.filter(
-            function(el){
-                if(el.sts == "recusado"){
-                    return el.sts
-                }
-            }
-        );
-        
-        //Cor Vermelha
-        var size3 = recusados.length;
-        //Caso não for status criado remove os itens de cor vermelha
-        if(item.id != "criado"){
-            size3 = 0
-        }else{
-            size1 -= size3;
-        }
-
-        //Cor Cinza
-        var size2 = items.length - (size1 + size3);
-
-        //Verificação de grafico, apos a primeira chamada o grafica passa a ficar depois
-        //de uma div para verificação de tamanho
-        var elem = item.children[0].tagName == "DIV" ? item.children[1] : item.children[0];
-        
-        createChart(elem, size1, size2, size3);
-    });
-    //Seleciona o primeiro item da lista
-    */
-    changeColor(listItems[0]);
-}
-
-/**
- * Método usado para criação de gráfico do tipo rosca
- * @param {HtmlObject} elem Objeto html em que ficará o gráfico
- * @param {Number} vl1 Valor exibido no gráfico com a cor verde
- * @param {Number} vl2 Valor exibido no gráfico com a cor cinza
- */
-function createChart(elem, valorVerde, valorCinza, valorVermelho) {
-    //Verifica se ha valor vermelho
-    if(!valorVermelho){valorVermelho = 0}
-    //validar se está recebendo elemento para criação do grafico
-    var ctx = elem != undefined ? elem.getContext('2d') : "";
-    if(ctx == ""){return;}
-
-    //Create Chart
-    var chart = new Chart(ctx, {
-        type: 'doughnut', //tipo do grafico
-        data: {
-            datasets: [{
-                data: [
-                    valorVerde, //Quantidade de itens de cor verde
-                    valorCinza, //Quantidade de itens de cor cinza
-                    valorVermelho  //Quantidade de itens de cor vermelha
-                ],
-                backgroundColor: [
-                    '#3ada3a', //verde
-                    '#aaa', //cinza
-                    '#f02929' //Vermelho
-                ],
-                borderWidth: 0, //tamanho da borda
-                weight: 1
-            }]
-            /*,
-            labels: [
-                'Completo',
-                'Pendente'
-            ]*/
-        },
-        options: {
-            cutoutPercentage: 70, //Quanto maior o valor menor a largura do grafico
-            responsive: true, //Ajustar automaticamente ao tamanho do elemento pai
-            tooltips: {
-                enabled: false //Desativa tooltips pois não são necessários
-            }
-        }
-    });
-}
-
-function getItems(){
-    return [
-        {
-            material: 'material 001',
-            sts: 'ativo',
-            dataSep: '12/12/2018 12:00:00',
-            vendedor: 'Marcelo Montalvão',
-            numNFE: 12345,
-            dataNFE: '13/12/2018 15:00:00',
-            dataMinuta: '15/12/2018 09:30:00',
-            dataRecebimento: '15/12/2018 11:30:00',
-            status: 'criado'
-        },{
-            material: 'material 002',
-            sts: 'ativo',
-            dataSep: '12/12/2018 12:00:00',
-            vendedor: 'Marcelo Montalvão',
-            numNFE: 12345,
-            dataNFE: '13/12/2018 15:00:00',
-            dataMinuta: '15/12/2018 09:30:00',
-            dataRecebimento: '15/12/2018 11:30:00',
-            status: 'validacao'
-        },{
-            material: 'material 003',
-            sts: 'ativo',
-            dataSep: '12/12/2018 12:00:00',
-            vendedor: 'Marcelo Montalvão',
-            numNFE: 12345,
-            dataNFE: '13/12/2018 15:00:00',
-            dataMinuta: '15/12/2018 09:30:00',
-            dataRecebimento: '15/12/2018 11:30:00',
-            status: 'faturado'
-        },{
-            material: 'material 004',
-            sts: 'ativo',
-            dataSep: '12/12/2018 12:00:00',
-            vendedor: 'Marcelo Montalvão',
-            numNFE: 12345,
-            dataNFE: '13/12/2018 15:00:00',
-            dataMinuta: '15/12/2018 09:30:00',
-            dataRecebimento: '15/12/2018 11:30:00',
-            status: 'validacao'
-        },{
-            material: 'material 005',
-            sts: 'ativo',
-            dataSep: '12/12/2018 12:00:00',
-            vendedor: 'Marcelo Montalvão',
-            numNFE: 12345,
-            dataNFE: '13/12/2018 15:00:00',
-            dataMinuta: '15/12/2018 09:30:00',
-            dataRecebimento: '15/12/2018 11:30:00',
-            status: 'separacao'
-        },{
-            material: 'material 006',
-            sts: 'recusado',
-            dataSep: '',
-            vendedor: 'Marcelo Montalvão',
-            dataMinuta: '',
-            dataRecebimento: '',
-            numNFE: '',
-            dataNFE: '',
-            status: 'criado'
-        }
-    ];
 }
