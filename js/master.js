813552300046d7a38c44bb7cfc6942caebf4a87d
Marcelo-RM <marcelordrgs98@gmail.com>
2019-11-11 15:30:38 -0200

    toast message

--- js/master.js
+++ js/master.js
@@ -126,9 +126,10 @@ function createListItem(item, status){
  */
 function updateChart(){
     var cnpj = $("#cnpj")[0];
-    if(cnpj.value == ""){
+    if(!_cnpj(cnpj.value)){
         cnpj.style.border = '1px solid red';
-        //return;
+        toast("error");
+        return;
     }
     else {
         cnpj.style.border = '1px solid #ccc';
@@ -411,4 +412,77 @@ function detailForStatusCriado(item){
         "</div>" +
     "</div>" +
     "</li>";
+}
+
+function toast(value){
+    if(value == "error")
+    $.toast({
+        heading: 'Error',
+        text: 'Cnpj Inválido!',
+        icon: 'error',
+        loader: true,
+        loaderBg: '#fff',
+        showHideTransition: 'plain',
+        hideAfter: 4000,
+        position: {
+          left: '40%',
+          top: 30
+        }
+    })
+}
+
+/*
+ * Método usado para verificar se o cnpj inserido é valido
+ */
+function _cnpj(cnpj) {
+
+    cnpj = cnpj.replace(/[^\d]+/g, '');
+
+    if (cnpj == '') {
+        return false;
+    }
+
+    if (cnpj.length != 14) {
+        return false;
+    }
+
+    if (cnpj == "00000000000000" || cnpj == "11111111111111" ||
+        cnpj == "22222222222222" || cnpj == "33333333333333" ||
+        cnpj == "44444444444444" || cnpj == "55555555555555" ||
+        cnpj == "66666666666666" || cnpj == "77777777777777" ||
+        cnpj == "88888888888888" || cnpj == "99999999999999") {
+        return false;
+    }
+
+
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0, tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+
+    if (resultado != digitos.charAt(0)) { return false; }
+
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0, tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2) {
+            pos = 9;
+        }
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1)) {
+        return false;
+    }
+    return true;
 }
